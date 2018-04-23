c2c939f1aaf5ef19fd73e82763c99cd4d7a57693
JaredMorales <jaredmorales@mail.weber.edu>
2018-04-22 21:46:43 -0600

    Update MainWindow.xaml.cs

--- GroupAssignment/Main/MainWindow.xaml.cs
+++ GroupAssignment/Main/MainWindow.xaml.cs
@@ -1,19 +1,9 @@
-﻿using GroupAssignment.Main;
 using GroupAssignment.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GroupAssignment
 {
@@ -42,7 +32,6 @@ namespace GroupAssignment
 
 
         }
-
 
 
 
@@ -253,6 +242,12 @@ namespace GroupAssignment
                 DbHandler.DeleteInvoice(invoiceId);
                 GetInvoices();
             }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var searchWindow = new Search();
+            searchWindow.ShowDialog();
         }
     }
 }
